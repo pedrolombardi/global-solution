f76cb46cd21e0ac0929f2a5b60099bfc0fe6f309
Lucas Vaz <lucasvazdev@gmail.com>
2023-11-24 16:31:17 -0300

    Função JS 1: Header animado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-// Função 1:
-
-// Função 2:
+// Função 1: animação header com scroll
+window.onscroll = function() {
+    toggleHeader()
+  };
+function toggleHeader () {
+    var header = document.getElementById("header")
+    if (document.scrollingElement.scrollTop >= 30) {
+    header.classList.add('small-header')
+    } else {
+    header.classList.remove('small-header');
+    }
+}
+// Função 2: carrossel
 
 // Função 3:
 
 // Função 4:
-
-// Função 5:
